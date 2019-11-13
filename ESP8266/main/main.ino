2dc6b3896d001b0bf628ea509c9058f231bbd94f
jpare006 <jpare006@ucr.edu>
2019-11-12 18:37:05 -0800

    Write code to connect device to LAN

--- ESP8266/main/main.ino
+++ ESP8266/main/main.ino
@@ -2,17 +2,41 @@
  */
 
  #include <ESP8266WiFi.h>
- #include "main.h"
+ #include "secrets.h"
 
- const char* ssid = SECRET_SSID;
+ const char* ssid     = SECRET_SSID;
+ const char* password = SECRET_PASSWORD;
  
  
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
 
+  //Connect to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println("Connecting to ");
+  Serial.println(SECRET_SSID);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
 }
